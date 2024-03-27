a67be0fc49d7c0bd6da6e6e5ec044bab11662193
Ubiraci Domingos <ipsuporte@gmail.com>
2024-03-27 11:54:21 -0300

    Sengundo comando configurando o repositório

--- CalculosMonetarios/Program.cs
+++ CalculosMonetarios/Program.cs
@@ -9,33 +9,33 @@ namespace CalculosMonetarios
 {
     internal class Program
     {
-        
-        const decimal taxaDiariaMulta = 0.33M;        
+
+        const decimal taxaDiariaMulta = 0.33M;
         static void Main(string[] args)
         {
             DateTime dataVencimento = new DateTime(2023, 1, 24);
             Console.WriteLine("Dias de atraso: " + Convert.ToString(CalcularDiasAtraso(dataVencimento)));
-            Console.WriteLine("Percentual de multa: " + Convert.ToString(PercentualMulta(dataVencimento))+ "%");
-           
+            Console.WriteLine("Percentual de multa: " + Convert.ToString(PercentualMulta(dataVencimento)) + "%");
+
         }
-       private static Decimal PercentualMulta(DateTime dataVencimento)
-        
+        private static Decimal PercentualMulta(DateTime dataVencimento)
+
         {
             int diasAtraso = CalcularDiasAtraso(dataVencimento);
             if (diasAtraso > 60)
                 return 20M;
             return diasAtraso * taxaDiariaMulta;
-        }          
-        private static int CalcularDiasAtraso (DateTime dataVencimento)
-        {
-            DateTime dataAtual = DateTime.Now;         
-            int diasAtraso = dataAtual.Subtract(dataVencimento).Days;
-            if(diasAtraso < 0)
-                return 0;
-                
-                return diasAtraso;
-
-            Console.WriteLine();
         }
+        private static int CalcularDiasAtraso(DateTime dataVencimento)
+        {
+            DateTime dataAtual = DateTime.Now;
+            int diasAtraso = dataAtual.Subtract(dataVencimento).Days;
+            if (diasAtraso < 0)
+                return 0;
+
+            return diasAtraso;
+
+            
+        } 
     }
 }
