5fdacaaede8c7a60fb785bde7d4964cba40b6bc6
Ubiraci Domingos <ipsuporte@gmail.com>
2024-03-27 11:45:17 -0300

    Versão inicial do projeto

---
+++ CalculosMonetarios/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CalculosMonetarios
+{
+    internal class Program
+    {
+        
+        const decimal taxaDiariaMulta = 0.33M;        
+        static void Main(string[] args)
+        {
+            DateTime dataVencimento = new DateTime(2023, 1, 24);
+            Console.WriteLine("Dias de atraso: " + Convert.ToString(CalcularDiasAtraso(dataVencimento)));
+            Console.WriteLine("Percentual de multa: " + Convert.ToString(PercentualMulta(dataVencimento))+ "%");
+           
+        }
+       private static Decimal PercentualMulta(DateTime dataVencimento)
+        
+        {
+            int diasAtraso = CalcularDiasAtraso(dataVencimento);
+            if (diasAtraso > 60)
+                return 20M;
+            return diasAtraso * taxaDiariaMulta;
+        }          
+        private static int CalcularDiasAtraso (DateTime dataVencimento)
+        {
+            DateTime dataAtual = DateTime.Now;         
+            int diasAtraso = dataAtual.Subtract(dataVencimento).Days;
+            if(diasAtraso < 0)
+                return 0;
+                
+                return diasAtraso;
+
+            Console.WriteLine();
+        }
+    }
+}
